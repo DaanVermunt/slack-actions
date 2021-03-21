aa26d19159aedaf03df7bf6857c65c42d1f74c23
Daan Vermunt <daan@protium.digital>
2021-03-21 14:23:54 +0100

    new deploy

--- index.ts
+++ index.ts
@@ -6,15 +6,21 @@ try {
     const botOAuthSecret = core.getInput('bot-oauth-secret');
 
     const githubToken = core.getInput('github-token');
+    const payload = github.context.payload
     const octo = github.getOctokit(githubToken)
 
-    const branch = process.env.GITHUB_REF.split('/').slice(2).join('/')
+    const prNum = process.env.GITHUB_REF.split('/')[2] // refs/pull/134/merge
+
+    const branch = octo.pulls.get({
+        owner: payload.repository.owner.name,
+        repo: payload.repository.full_name,
+        pull_number: parseInt(prNum),
+    })
 
     switch (actionType) {
         case 'PR_OPEN':
-            console.log(`${process.env.GITHUB_REF}`)
-            console.log(`${process.env}`)
             console.log(`create channel ${branch}`)
+            console.log(JSON.stringify(branch))
             break
         case 'PR_CLOSE':
             console.log(`remove/archive channel ${branch}`)
