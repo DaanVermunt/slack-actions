a17fc8bded7c9dc44cced5c67fe74098d85f2f0f
Daan Vermunt <daan@protium.digital>
2021-03-21 13:47:49 +0100

    new deploy

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import * as core from '@actions/core'
-// import github from '@actions/github'
+import * as github from '@actions/github'
 
 try {
     // `who-to-greet` input defined in action metadata file
@@ -11,8 +11,8 @@ try {
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
     // Get the JSON webhook payload for the event that triggered the workflow
-    // const payload = JSON.stringify(github.context.payload, undefined, 2)
-    // console.log(`The event payload: ${payload}`);
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
 } catch (error) {
     core.setFailed(error.message);
 }
