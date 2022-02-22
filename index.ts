b2da91aae6b3f76873a8f3d60b4d50a55fb2d962
Daan Vermunt <daan@syntactics.dev>
2022-02-22 16:10:21 +0100

    new deploy

--- index.ts
+++ index.ts
@@ -3,8 +3,8 @@ import * as core from '@actions/core'
 import * as github from '@actions/github'
 
 const findChannel = async (client: WebClient, name: string) => {
+    console.log(await client.conversations.list())
     const listChannelResponse = await client.conversations.list()
-    console.log(listChannelResponse)
     const channels = listChannelResponse.channels as { id: string, name: string }[]
     const channel = channels.find(ch => ch.name === name)
 
