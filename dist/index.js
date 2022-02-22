45c558abb9b3046e4296183f0fdc4bdf26fb2bbe
Daan Vermunt <daan@syntactics.dev>
2022-02-22 16:11:22 +0100

    new deploy

--- dist/index.js
+++ dist/index.js
@@ -13950,7 +13950,8 @@ const web_api_1 = __webpack_require__(431);
 const core = __webpack_require__(2186);
 const github = __webpack_require__(5438);
 const findChannel = async (client, name) => {
-    console.log(await client.conversations.list());
+    const respones = await client.conversations.list();
+    console.log(respones);
     const listChannelResponse = await client.conversations.list();
     const channels = listChannelResponse.channels;
     const channel = channels.find(ch => ch.name === name);
